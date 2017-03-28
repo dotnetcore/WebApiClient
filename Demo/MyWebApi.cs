6b00ca28fd4815c38eb76fca66c79de509320775
xljiulang <366193849@qq.com>
2017-03-28 14:23:39 +0800

    webapi

---
+++ Demo/MyWebApi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient;
+
+namespace Demo
+{
+    [JsonReturn]
+    [HttpHost("http://www.mywebapi.com")]
+    public interface MyWebApi
+    {
+        [HttpGet("/webapi/{type}/about")] // GET webapi/typeValue/about
+        Task<ApiResult<string>> GetAboutAsync(string type);
+
+
+        [HttpGet("/webapi/user")]  // GET webapi/user?userName=aa&nickName=bb&&BeginTime=cc&EndTime=dd
+        Task<ApiResult<UserInfo>> GetUserAsync(string userName, string nickName, TimeFilter timeFilter);
+
+
+        [HttpPut("/webapi/user")] // PUT webapi/user
+        Task<ApiResult<bool>> UpdateUserAsync([JsonContent] UserInfo loginInfo);
+
+
+        [HttpDelete("/webapi/user")] // DELETE  webapi/user?id=idValue
+        Task<ApiResult<bool>> DeleteUserAsync(string id);
+
+
+        [HttpDelete("/webapi/user/{id}")] // DELETE  webapi/user/idValue
+        Task<ApiResult<bool>> DeleteUser2Async(string id);
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static async void Test()
+        {
+            var myWebApi = new WebApiClient.HttpApiClient().GetHttpApi<MyWebApi>();
+
+            await myWebApi.GetAboutAsync("typeValue");
+            await myWebApi.UpdateUserAsync(new UserInfo { UserName = "abc", Password = "123456" });
+            await myWebApi.DeleteUser2Async(id: "id001");
+        }
+
+        static void Main(string[] args)
+        {
+            Test();
+            Console.ReadLine();
+        }
+    }
+}
