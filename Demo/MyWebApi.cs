9431deee9b1feebbdad74f2775be4206dc54bd78
xljiulang <366193849@qq.com>
2017-09-11 22:58:33 +0800

    xml注释

--- Demo/MyWebApi.cs
+++ Demo/MyWebApi.cs
@@ -13,30 +13,30 @@ namespace Demo
     public interface MyWebApi
     {
         // GET webapi/user/id001
-        // Return Http回复对象
+        // Return HttpResponseMessage
         [HttpGet("/webapi/user/{id}")]
         Task<HttpResponseMessage> GetUserByIdAsync(string id);
 
         // GET webapi/user?account=laojiu
-        // Return json内容
+        // Return 原始string内容
         [HttpGet("/webapi/user")]
         Task<string> GetUserByAccountAsync(string account);
 
 
         // POST webapi/user  
         // Body:Account=laojiu&Password=123456
-        // Return json内容
+        // Return json或xml内容
         [HttpPost("/webapi/user")]
         Task<UserInfo> UpdateUserWithFormAsync([FormContent] UserInfo user);
 
         // POST webapi/user   
         // Body:{"Account":"laojiu","Password":"123456"}
-        // Return json内容
+        // Return json或xml内容
         [HttpPost("/webapi/user")]
         Task<UserInfo> UpdateUserWithJsonAsync([JsonContent] UserInfo user);
 
         // POST webapi/user   
-        // Body: xml内容
+        // Body:﻿<?xml version="1.0" encoding="utf-8"?><UserInfo><Account>laojiu</Account><Password>123456</Password></UserInfo>
         // Return xml内容
         [XmlReturn]
         [HttpPost("/webapi/user")]
