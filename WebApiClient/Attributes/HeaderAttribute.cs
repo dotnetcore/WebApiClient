f8cfcb541f486845aa5d06e744eb1e196d0b2bb2
xljiulang <366193849@qq.com>
2017-09-14 23:30:59 +0800

    增加HeaderAttribute和UrlAttribute

---
+++ WebApiClient/Attributes/HeaderAttribute.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient.Contexts;
+
+namespace WebApiClient.Attributes
+{
+    /// <summary>
+    /// 表示Http请求Header的特性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Parameter, AllowMultiple = true, Inherited = true)]
+    public class HeaderAttribute : Attribute, IApiActionAttribute, IApiParameterAttribute
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        private readonly string name;
+
+        /// <summary>
+        /// 值 
+        /// </summary>
+        private readonly string value;
+
+        /// <summary>
+        /// 将参数值设置到Header
+        /// </summary>
+        /// <param name="name">header名称</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public HeaderAttribute(string name)
+            : this(name, null)
+        {
+        }
+
+        /// <summary>
+        /// 将指定值设置到Header
+        /// </summary>
+        /// <param name="name">header名称</param>
+        /// <param name="value">header值</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public HeaderAttribute(string name, string value)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+            this.name = name;
+            this.value = value;
+        }
+
+        /// <summary>
+        /// 执行前
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <returns></returns>
+        Task IApiActionAttribute.BeforeRequestAsync(ApiActionContext context)
+        {
+            var header = context.RequestMessage.Headers;
+            header.Remove(this.name);
+            header.Add(this.name, this.value);
+            return TaskExtend.CompletedTask;
+        }
+
+        /// <summary>
+        /// http请求之前
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <param name="parameter">特性关联的参数</param>
+        /// <returns></returns>
+        Task IApiParameterAttribute.BeforeRequestAsync(ApiActionContext context, ApiParameterDescriptor parameter)
+        {
+            var header = context.RequestMessage.Headers;
+            var valueString = parameter.Value == null ? null : parameter.Value.ToString();
+            header.Remove(this.name);
+            header.Add(this.name, valueString);
+            return TaskExtend.CompletedTask;
+        }
+
+        /// <summary>
+        /// 转换为字符串
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.name;
+        }
+    }
+}
---
+++ WebApiClient/ParameterAttributes/UrlAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient.Contexts;
+
+namespace WebApiClient.Attributes
+{
+    /// <summary>
+    /// 表示将参数的值作为请求Url的特性
+    /// 支持绝对或相对路径
+    /// 一般放到第一个参数以防止将PathQuery的路径覆盖掉
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+    public class UrlAttribute : ApiParameterAttribute
+    {
+        /// <summary>
+        /// http请求之前
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <param name="parameter">特性关联的参数</param>
+        /// <returns></returns>
+        public override Task BeforeRequestAsync(ApiActionContext context, ApiParameterDescriptor parameter)
+        {
+            if (parameter.Value == null)
+            {
+                throw new ArgumentNullException(parameter.Name);
+            }
+
+            var url = new Uri(parameter.Value.ToString(), UriKind.RelativeOrAbsolute);
+            if (url.IsAbsoluteUri == true)
+            {
+                context.RequestMessage.RequestUri = url;
+            }
+            else
+            {
+                var baseUrl = context.RequestMessage.RequestUri;
+                context.RequestMessage.RequestUri = new Uri(baseUrl, url);
+            }
+            return TaskExtend.CompletedTask;
+        }
+    }
+}
