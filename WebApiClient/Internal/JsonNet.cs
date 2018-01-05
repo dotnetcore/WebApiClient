3705a6d48ab69861a774b34a5e79120439f40ad4
xljiulang <366193849@qq.com>
2018-01-06 01:13:34 +0800

    JsonNet反射优化

--- WebApiClient/Internal/JsonNet.cs
+++ WebApiClient/Internal/JsonNet.cs
@@ -24,14 +24,14 @@ namespace WebApiClient
         private static readonly string assemblyFile = assemblyName + ".dll";
 
         /// <summary>
-        /// 序列化方法
+        /// 序列化方法的委托
         /// </summary>
-        private static readonly Method serializeMethod = null;
+        private static readonly Func<object, string> serializeFunc = null;
 
         /// <summary>
-        /// 反序列化方法
+        /// 反序列化方法的委托
         /// </summary>
-        private static readonly Method deserializeMethod = null;
+        private static readonly Func<string, Type, object> deserializeFunc = null;
 
         /// <summary>
         /// 获取是否得到支持
@@ -46,10 +46,10 @@ namespace WebApiClient
             var type = FindJsonConvertType();
             if (type != null)
             {
-                serializeMethod = FindSerializeObjectMethod(type);
-                deserializeMethod = FindDeserializeObjectMethod(type);
+                serializeFunc = FindSerializeObjectFunc(type);
+                deserializeFunc = FindDeserializeObjectFunc(type);
             }
-            IsSupported = serializeMethod != null && deserializeMethod != null;
+            IsSupported = serializeFunc != null && deserializeFunc != null;
         }
 
         /// <summary>
@@ -59,7 +59,7 @@ namespace WebApiClient
         /// <returns></returns>
         public static string SerializeObject(object obj)
         {
-            return serializeMethod.Invoke(null, obj) as string;
+            return serializeFunc.Invoke(obj) as string;
         }
 
         /// <summary>
@@ -70,7 +70,7 @@ namespace WebApiClient
         /// <returns></returns>
         public static object DeserializeObject(string json, Type type)
         {
-            return deserializeMethod.Invoke(null, json, type);
+            return deserializeFunc.Invoke(json, type);
         }
 
         /// <summary>
@@ -99,31 +99,31 @@ namespace WebApiClient
         /// <summary>
         /// 查找SerializeObject方法
         /// </summary>
-        /// <param name="type">JsonConvert</param>
+        /// <param name="classType">JsonConvert</param>
         /// <returns></returns>
-        private static Method FindSerializeObjectMethod(Type type)
+        private static Func<object, string> FindSerializeObjectFunc(Type classType)
         {
-            var method = type.GetMethod("SerializeObject", new[] { typeof(object) });
+            var method = classType.GetMethod("SerializeObject", new[] { typeof(object) });
             if (method == null)
             {
                 return null;
             }
-            return new Method(method);
+            return (Func<object, string>)method.CreateDelegate(typeof(Func<object, string>));
         }
 
         /// <summary>
         /// 查找DeserializeObject方法
         /// </summary>
-        /// <param name="type">JsonConvert</param>
+        /// <param name="classType">JsonConvert</param>
         /// <returns></returns>
-        private static Method FindDeserializeObjectMethod(Type type)
+        private static Func<string, Type, object> FindDeserializeObjectFunc(Type classType)
         {
-            var method = type.GetMethod("DeserializeObject", new[] { typeof(string), typeof(Type) });
+            var method = classType.GetMethod("DeserializeObject", new[] { typeof(string), typeof(Type) });
             if (method == null)
             {
                 return null;
             }
-            return new Method(method);
+            return (Func<string, Type, object>)method.CreateDelegate(typeof(Func<string, Type, object>));
         }
     }
 }
