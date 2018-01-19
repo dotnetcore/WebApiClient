ecccae73b182a27f45b6038a440b1c398de2415f
xljiulang <366193849@qq.com>
2018-01-19 17:16:24 +0800

    dynamicConverter

---
+++ WebApiClient/Defaults/KeyValueFormats/Converters/DynamicObjectConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiClient.Defaults.KeyValueFormats.Converters
+{
+    /// <summary>
+    /// 表示动态类型转换器
+    /// </summary>
+    public class DynamicObjectConverter : ConverterBase
+    {
+        /// <summary>
+        /// 执行转换
+        /// </summary>
+        /// <param name="context">转换上下文</param>
+        /// <returns></returns>
+        public override IEnumerable<KeyValuePair<string, string>> Invoke(ConvertContext context)
+        {
+            var dynamicObject = context.Value as DynamicObject;
+            if (dynamicObject != null)
+            {
+                return from name in dynamicObject.GetDynamicMemberNames()
+                       let value = this.GetValue(dynamicObject, name)
+                       select base.ConvertToKeyValuePair(name, value, context.Options);
+            }
+
+            return this.Next.Invoke(context);
+        }
+
+        /// <summary>
+        /// 获取动态类型的值
+        /// </summary>
+        /// <param name="dynamicObject">实例</param>
+        /// <param name="name">名称</param>
+        /// <returns></returns>
+        private object GetValue(DynamicObject dynamicObject, string name)
+        {
+            object value;
+            var binder = new MemberBinder(name);
+            dynamicObject.TryGetMember(binder, out value);
+            return value;
+        }
+
+        /// <summary>
+        /// 表示成员值的获取绑定
+        /// </summary>
+        private class MemberBinder : GetMemberBinder
+        {
+            /// <summary>
+            /// 键的信息获取绑定
+            /// </summary>
+            /// <param name="key">键名</param>
+            public MemberBinder(string key)
+                : base(key, false)
+            {
+            }
+
+            /// <summary>
+            /// 在派生类中重写时，如果无法绑定目标动态对象，则执行动态获取成员操作的绑定
+            /// </summary>
+            /// <param name="target"></param>
+            /// <param name="errorSuggestion"></param>
+            /// <returns></returns>
+            public override DynamicMetaObject FallbackGetMember(DynamicMetaObject target, DynamicMetaObject errorSuggestion)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
