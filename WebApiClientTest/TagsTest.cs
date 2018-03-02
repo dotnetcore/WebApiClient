9effb2d386041abd8abc421bb6e0545e8f22983e
xljiulang <366193849@qq.com>
2018-03-02 23:01:20 +0800

    增加单元测试项目

---
+++ WebApiClientTest/TagsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+using WebApiClient;
+
+namespace WebApiClientTest
+{
+    public class TagsTest
+    {
+        [Fact]
+        public void Test_All_Methods()
+        {
+            var tags = new Tags();
+            tags.Id = "id";
+            tags.Set("string", "a");
+            tags.Set("int", 1);
+            tags.Set("class", new TagsTest());
+
+            Assert.True(tags.Id == "id");
+            Assert.True(tags["string"].As<string>() == "a");
+            Assert.True(tags.Get("int").AsInt32() == 1);
+            Assert.True(tags.Get("class").As<TagsTest>() != null);
+
+            tags.Remove("class");
+            Assert.True(tags.Get("class").IsNull);
+        }
+    }
+}
