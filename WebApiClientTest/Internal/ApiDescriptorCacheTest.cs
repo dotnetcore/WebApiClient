0d02953f310d07ef15cbbf6f340a9bf813e76a28
xljiulang <366193849@qq.com>
2018-03-05 15:59:56 +0800

    增加更多类型的单元测试

---
+++ WebApiClientTest/Internal/ApiDescriptorCacheTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+using WebApiClient;
+using WebApiClient.Interfaces;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient.Attributes;
+
+namespace WebApiClientTest.Internal
+{
+    public class ApiDescriptorCacheTest
+    {
+        public interface IMyApi : IHttpApiClient
+        {
+            ITask<string> Login(int a);
+        }
+
+        [Fact]
+        public void GetApiActionDescriptorTest()
+        {
+            var method = typeof(IMyApi).GetMethod("Login");
+            var descriptor1 = ApiDescriptorCache.GetApiActionDescriptor(method);
+            var descriptor2 = ApiDescriptorCache.GetApiActionDescriptor(method);
+            Assert.True(object.ReferenceEquals(descriptor1, descriptor2));
+
+            Assert.True(descriptor1.Name == "Login");
+            Assert.True(descriptor1.Parameters.Length == 1);
+            Assert.True(descriptor1.Filters.Length == 0);
+            Assert.True(descriptor1.Attributes.Length == 0);
+        }
+    }
+}
---
+++ WebApiClientTest/Internal/PropertySetterTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApiClient;
+using Xunit;
+
+namespace WebApiClientTest.Internal
+{
+    public class PropertySetterTest
+    {
+        [Fact]
+        public void InvokeTest()
+        {
+            var model = new Model { name = "laojiu" };
+            var setter = new PropertySetter(model.GetType().GetProperty("name"));
+            setter.Invoke(model, "ee");
+            Assert.True("ee" == model.name);
+        }
+
+        class Model
+        {
+            public string name { get; set; }
+        }
+    }
+}
