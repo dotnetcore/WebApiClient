758cdc876ed37c29c0978e3184f197ead3ff1b9f
xljiulang <366193849@qq.com>
2018-03-06 19:16:20 +0800

    AutoReturnAttributeTest完整性

--- WebApiClientTest/Attributes/ReturnAttributes/AutoReturnAttributeTest.cs
+++ WebApiClientTest/Attributes/ReturnAttributes/AutoReturnAttributeTest.cs
@@ -17,10 +17,20 @@ namespace WebApiClientTest.Attributes.HttpActionAttributes
             ITask<HttpResponseMessage> HttpResponseMessageAsync();
 
             ITask<string> StringAsync();
+
+            ITask<byte[]> ByteArrayAsync();
+
+            ITask<Model> JsonXmlAsync();
+        }
+
+        public class Model
+        {
+            public string Name { get; set; } = "laojiu";
+            public int Age { get; set; } = 18;
         }
 
         [Fact]
-        public async Task HttpResponseMessageAsyncTest()
+        public async Task HttpResponseMessageResultTest()
         {
             var context = new ApiActionContext
             {
@@ -43,7 +53,7 @@ namespace WebApiClientTest.Attributes.HttpActionAttributes
         }
 
         [Fact]
-        public async Task StringAsyncTest()
+        public async Task StringResultTest()
         {
             var context = new ApiActionContext
             {
@@ -63,6 +73,82 @@ namespace WebApiClientTest.Attributes.HttpActionAttributes
             var attr = new AutoReturnAttribute();
             var result = await ((IApiReturnAttribute)attr).GetTaskResult(context);
             Assert.True(result?.ToString() == "laojiu");
-        }        
+        }
+
+        [Fact]
+        public async Task ByteArrayResultTest()
+        {
+            var context = new ApiActionContext
+            {
+                HttpApiConfig = new HttpApiConfig(),
+                RequestMessage = new HttpApiRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri("http://www.webapi.com/")
+                },
+                ResponseMessage = new HttpResponseMessage(System.Net.HttpStatusCode.OK),
+                ApiActionDescriptor = ApiDescriptorCache
+                .GetApiActionDescriptor(typeof(IMyApi)
+                .GetMethod("ByteArrayAsync"))
+            };
+            context.ResponseMessage.Content = new StringContent("laojiu", Encoding.UTF8);
+
+            var attr = new AutoReturnAttribute();
+            var result = await ((IApiReturnAttribute)attr).GetTaskResult(context);
+            var text = Encoding.UTF8.GetString((byte[])result);
+            Assert.True(text == "laojiu");
+        }
+
+        [Fact]
+        public async Task JsonResultTest()
+        {
+            var context = new ApiActionContext
+            {
+                HttpApiConfig = new HttpApiConfig(),
+                RequestMessage = new HttpApiRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri("http://www.webapi.com/")
+                },
+                ResponseMessage = new HttpResponseMessage(System.Net.HttpStatusCode.OK),
+                ApiActionDescriptor = ApiDescriptorCache
+                .GetApiActionDescriptor(typeof(IMyApi)
+                .GetMethod("JsonXmlAsync"))
+            };
+
+            var model = new Model();
+            var json = context.HttpApiConfig.JsonFormatter.Serialize(model, context.HttpApiConfig.FormatOptions);
+            context.ResponseMessage.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var attr = new AutoReturnAttribute();
+            var result = await ((IApiReturnAttribute)attr).GetTaskResult(context) as Model;
+            Assert.True(model.Name == result.Name && model.Age == result.Age);
+        }
+
+        [Fact]
+        public async Task XmlResultTest()
+        {
+            var context = new ApiActionContext
+            {
+                HttpApiConfig = new HttpApiConfig(),
+                RequestMessage = new HttpApiRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri("http://www.webapi.com/")
+                },
+                ResponseMessage = new HttpResponseMessage(System.Net.HttpStatusCode.OK),
+                ApiActionDescriptor = ApiDescriptorCache
+                .GetApiActionDescriptor(typeof(IMyApi)
+                .GetMethod("JsonXmlAsync"))
+            };
+
+            var model = new Model();
+            var xml = context.HttpApiConfig.XmlFormatter.Serialize(model, Encoding.UTF8);
+            context.ResponseMessage.Content = new StringContent(xml, Encoding.UTF8, "application/xml");
+
+            var attr = new AutoReturnAttribute();
+            var result = await ((IApiReturnAttribute)attr).GetTaskResult(context) as Model;
+            Assert.True(model.Name == result.Name && model.Age == result.Age);
+        }
     }
 }
