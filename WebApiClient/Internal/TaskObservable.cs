74c34760d3b7eb908bc6077d087e0058e863fd60
xljiulang <366193849@qq.com>
2018-04-09 19:28:46 +0800

    合并TaskObserver类

--- WebApiClient/Internal/TaskObservable.cs
+++ WebApiClient/Internal/TaskObservable.cs
@@ -226,5 +226,66 @@ namespace WebApiClient
                 this.onUnsubscribe?.Invoke();
             }
         }
+
+        /// <summary>
+        /// 表示任务的观察者
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private class TaskObserver<T> : IObserver<T>
+        {
+            /// <summary>
+            /// 有值更新委托
+            /// </summary>
+            private readonly Action<T> onNext;
+
+            /// <summary>
+            /// 错误委托
+            /// </summary>
+            private readonly Action<Exception> onError;
+
+            /// <summary>
+            /// 完成委托
+            /// </summary>
+            private readonly Action onCompleted;
+
+            /// <summary>
+            /// 任务的观察者
+            /// </summary>
+            /// <param name="onNext">有值更新委托</param>
+            /// <param name="onError">错误委托</param>
+            /// <param name="onCompleted">完成委托</param>
+            public TaskObserver(Action<T> onNext, Action<Exception> onError, Action onCompleted)
+            {
+                this.onNext = onNext;
+                this.onError = onError;
+                this.onCompleted = onCompleted;
+            }
+
+            /// <summary>
+            /// 完成触发
+            /// </summary>
+            public void OnCompleted()
+            {
+                this.onCompleted?.Invoke();
+            }
+
+            /// <summary>
+            /// 错误触发
+            /// </summary>
+            /// <param name="error"></param>
+            public void OnError(Exception error)
+            {
+                this.onError?.Invoke(error);
+            }
+
+            /// <summary>
+            /// 有值更新触发
+            /// </summary>
+            /// <param name="value"></param>
+            public void OnNext(T value)
+            {
+                this.onNext?.Invoke(value);
+            }
+        }
     }
 }
