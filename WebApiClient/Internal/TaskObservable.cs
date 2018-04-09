09a34a4c6246e063ecb63cef1862de2d7dae6039
xljiulang <366193849@qq.com>
2018-04-09 17:28:51 +0800

    Rx bug修复

--- WebApiClient/Internal/TaskObservable.cs
+++ WebApiClient/Internal/TaskObservable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebApiClient
@@ -11,14 +12,14 @@ namespace WebApiClient
     class TaskObservable<TResult> : IObservable<TResult>
     {
         /// <summary>
-        /// 同步锁
+        /// 任务
         /// </summary>
-        private readonly object syncRoot = new object();
+        private readonly Task<TResult> task;
 
         /// <summary>
         /// 观察者列表
         /// </summary>
-        private readonly List<IObserver<TResult>> observerList = new List<IObserver<TResult>>();
+        private readonly ObserverTable<TResult> observerTable = new ObserverTable<TResult>();
 
         /// <summary>
         /// 任务的Rx
@@ -26,43 +27,56 @@ namespace WebApiClient
         /// <param name="task">任务</param>
         public TaskObservable(Task<TResult> task)
         {
-            task.ContinueWith(t =>
+            this.task = task;
+            this.task.ContinueWith(this.OnTaskCompleted);
+        }
+
+        /// <summary>
+        /// 任务完成时
+        /// </summary>
+        /// <param name="t"></param>
+        private void OnTaskCompleted(Task<TResult> t)
+        {
+            switch (t.Status)
             {
-                switch (t.Status)
-                {
-                    case TaskStatus.RanToCompletion:
-                        lock (this.syncRoot)
-                        {
-                            foreach (var observer in this.observerList)
-                            {
-                                observer.OnNext(t.Result);
-                                observer.OnCompleted();
-                            }
-                            this.observerList.Clear();
-                        }
-                        break;
+                case TaskStatus.RanToCompletion:
+                    this.observerTable.RaiseNextAndCompleted(t.Result);
+                    break;
 
-                    case TaskStatus.Faulted:
-                        lock (this.syncRoot)
-                        {
-                            foreach (var observer in this.observerList)
-                            {
-                                observer.OnError(t.Exception);
-                            }
-                        }
-                        break;
+                case TaskStatus.Faulted:
+                    this.observerTable.RaiseErrorAndCompleted(t.Exception.InnerException);
+                    break;
 
-                    case TaskStatus.Canceled:
-                        lock (this.syncRoot)
-                        {
-                            foreach (var observer in this.observerList)
-                            {
-                                observer.OnError(new TaskCanceledException(t));
-                            }
-                        }
-                        break;
-                }
-            });
+                case TaskStatus.Canceled:
+                    this.observerTable.RaiseErrorAndCompleted(new TaskCanceledException(t));
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 响应观察者
+        /// </summary>
+        /// <param name="observer"></param>
+        /// <param name="t"></param>
+        private void RaiseObserver(IObserver<TResult> observer, Task<TResult> t)
+        {
+            switch (t.Status)
+            {
+                case TaskStatus.RanToCompletion:
+                    observer.OnNext(t.Result);
+                    observer.OnCompleted();
+                    break;
+
+                case TaskStatus.Faulted:
+                    observer.OnError(t.Exception.InnerException);
+                    observer.OnCompleted();
+                    break;
+
+                case TaskStatus.Canceled:
+                    observer.OnError(new TaskCanceledException(t));
+                    observer.OnCompleted();
+                    break;
+            }
         }
 
         /// <summary>
@@ -72,18 +86,104 @@ namespace WebApiClient
         /// <returns></returns>
         public IDisposable Subscribe(IObserver<TResult> observer)
         {
-            lock (this.syncRoot)
+            if (this.observerTable.Add(observer) == true)
             {
-                this.observerList.Add(observer);
+                return new Unsubscriber<TResult>(() => this.observerTable.Remove(observer));
             }
+            else
+            {
+                this.RaiseObserver(observer, this.task);
+                return new Unsubscriber<TResult>(null);
+            }
+        }
 
-            return new Unsubscriber<TResult>(() =>
+        /// <summary>
+        /// 表示观察者列表
+        /// 线程安全类型 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private class ObserverTable<T>
+        {
+            /// <summary>
+            /// 是否触发过
+            /// </summary>
+            private bool raised = false;
+
+            /// <summary>
+            /// 同步锁
+            /// </summary>
+            private readonly object syncRoot = new object();
+
+            /// <summary>
+            /// 观察者列表
+            /// </summary>
+            private readonly List<IObserver<T>> observers = new List<IObserver<T>>();
+
+            /// <summary>
+            /// 添加观察者
+            /// </summary>
+            /// <param name="observer">观察者</param>
+            /// <returns></returns>
+            public bool Add(IObserver<T> observer)
             {
                 lock (this.syncRoot)
                 {
-                    this.observerList.Remove(observer);
+                    if (this.raised == false)
+                    {
+                        this.observers.Add(observer);
+                        return true;
+                    }
+                    return false;
                 }
-            });
+            }
+
+            /// <summary>
+            /// 移除观察者
+            /// </summary>
+            /// <param name="observer">观察者</param>
+            public void Remove(IObserver<T> observer)
+            {
+                lock (this.syncRoot)
+                {
+                    this.observers.Remove(observer);
+                }
+            }
+
+            /// <summary>
+            /// 触发有结果值到所有观察者
+            /// </summary>
+            /// <param name="value">结果值</param>
+            public void RaiseNextAndCompleted(T value)
+            {
+                lock (this.syncRoot)
+                {
+                    this.raised = true;
+                    foreach (var item in this.observers)
+                    {
+                        item.OnNext(value);
+                        item.OnCompleted();
+                    }
+                    this.observers.Clear();
+                }
+            }
+
+            /// <summary>
+            /// 触发有异常到所有观察者
+            /// </summary>
+            /// <param name="ex">异常</param>
+            public void RaiseErrorAndCompleted(Exception ex)
+            {
+                lock (this.syncRoot)
+                {
+                    this.raised = true;
+                    foreach (var item in this.observers)
+                    {
+                        item.OnError(ex);
+                        item.OnCompleted();
+                    }
+                    this.observers.Clear();
+                }
+            }
         }
 
         /// <summary>
@@ -111,7 +211,7 @@ namespace WebApiClient
             /// </summary>
             public void Dispose()
             {
-                this.onUnsubscribe.Invoke();
+                this.onUnsubscribe?.Invoke();
             }
         }
     }
