af026187c209841e17cc01212ae66e11cf6aa3dd
xljiulang <366193849@qq.com>
2018-05-10 15:48:27 +0800

    添加UPW AOT的支持

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace App1
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var client = WebApiClient.HttpApiClient.Create<IBaiduApi>();
+                var str = await client.GetAsync("https://www.baidu.com/");
+                this.Btn.Content = str.Substring(0, 5);
+            }
+            catch (Exception ex)
+            {
+                this.Btn.Content = ex.Message;
+            }
+        }
+    }
+}
--- ProxyGenarater/Program.cs
+++ ProxyGenarater/Program.cs
@@ -11,41 +11,8 @@ namespace ProxyGenarater
         static void Main(string[] args)
         {
             var fileName = args.FirstOrDefault();
-            var files = SearchFiles(fileName, ".dll", ".exe");
-
-            foreach (var item in files)
-            {
-                var proxyBuilder = new ProxyBuilder(item);
-                 proxyBuilder.BuildAndSave();
-            }
-        }
-
-        /// <summary>
-        /// 搜索文件
-        /// </summary>
-        /// <param name="fileName">文件</param>
-        /// <param name="extension">可能的扩展名</param>
-        /// <returns></returns>
-        private static IEnumerable<string> SearchFiles(string fileName, params string[] extension)
-        {
-            if (string.IsNullOrEmpty(fileName) == true)
-            {
-                yield break;
-            }
-
-            if (File.Exists(fileName) == true)
-            {
-                yield return fileName;
-            }
-
-            foreach (var item in extension)
-            {
-                var file = Path.ChangeExtension(fileName, item);
-                if (File.Exists(file) == true)
-                {
-                    yield return file;
-                }
-            }
-        }
+            var proxyBuilder = new ProxyBuilder(fileName);
+            proxyBuilder.BuildAndSave();
+        }        
     }
 }
