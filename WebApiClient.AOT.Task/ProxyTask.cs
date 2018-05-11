f34f89d0cfc19b0382d6070d9389ad91355451af
xljiulang <366193849@qq.com>
2018-05-11 15:25:41 +0800

    集成MSBuild

---
+++ WebApiClient.AOT.Task/ProxyTask.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Build.Framework;
+using System;
+
+namespace WebApiClient.AOT.Task
+{
+    /// <summary>
+    /// 代理任务
+    /// </summary>
+    public class ProxyTask : Microsoft.Build.Utilities.Task
+    {
+        /// <summary>
+        /// 插入代理的程序集
+        /// </summary>
+        [Required]
+        public string ProxyAssembly { get; set; }
+
+        /// <summary>
+        /// 执行任务
+        /// </summary>
+        /// <returns></returns>
+        public override bool Execute()
+        {
+            try
+            {
+                using (var assembly = new Assembly(this.ProxyAssembly))
+                {
+                    assembly.WirteProxyTypes();
+                    assembly.Save();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+    }
+}
