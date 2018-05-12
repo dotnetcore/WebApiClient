b1d7aec7c4f6b7dcb0c1aed12a82b07a2051c65d
xljiulang <366193849@qq.com>
2018-05-13 00:11:17 +0800

    增加WebApiClient.AOT.Task发布批处理

---
+++ WebApiClient.AOT.Task/pulish.bat
@@ -0,0 +1,2 @@
+dotnet publish -c AOT_Release -f net46 -o ..\WebApiClient\MsBuild\BuildTask\net46\
+dotnet publish -c AOT_Release -f netcoreapp2.0 -o ..\WebApiClient\MsBuild\BuildTask\netcoreapp2.0\
