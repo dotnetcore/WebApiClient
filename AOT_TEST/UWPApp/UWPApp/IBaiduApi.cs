090200ae1b9db78235f881ed843bc85f65434efc
xljiulang <366193849@qq.com>
2018-05-13 01:35:14 +0800

    uwp app demo

---
+++ AOT_TEST/UWPApp/UWPApp/IBaiduApi.cs
@@ -0,0 +1,13 @@
+﻿using WebApiClient;
+using WebApiClient.Attributes;
+using WebApiClient.DataAnnotations;
+
+namespace UWPApp
+{
+    [HttpHost("https://www.baidu.com/")]
+    interface IBaiduApi : IHttpApi
+    {
+        [HttpGet("/")]
+        ITask<string> HomeAsync();
+    }
+}
---
+++ AOT_TEST/UWPApp/UWPApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using WebApiClient;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace UWPApp
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var client = HttpApiClient.Create<IBaiduApi>();
+            var html = await client.HomeAsync();
+            this.Html.Text += html;
+        }
+    }
+}
