8320586cbb33b84be2e7a72e638cbe2d48069de0
xljiulang <366193849@qq.com>
2018-05-13 02:23:34 +0800

    增加nuget打包命令

--- WebApiClient.AOT.Task/pulish.bat
+++
@@ -1,2 +0,0 @@
-dotnet publish -c AOT_Release -f net46 -o ..\WebApiClient\MsBuild\BuildTask\net46\
-dotnet publish -c AOT_Release -f netcoreapp2.0 -o ..\WebApiClient\MsBuild\BuildTask\netcoreapp2.0\
---
+++ nuget.build.bat
@@ -0,0 +1,14 @@
+cd WebApiClient.AOT.Task
+dotnet publish -v q -c AOT_Release -f net46 -o ..\WebApiClient\MsBuild\BuildTask\net46\
+dotnet publish -v q -c AOT_Release -f netcoreapp2.0 -o ..\WebApiClient\MsBuild\BuildTask\netcoreapp2.0\
+
+cd ..\WebApiClient
+dotnet build -c AOT_Release -f net45
+dotnet build -c AOT_Release -f netcoreapp2.1
+dotnet build -c AOT_Release -f netstandard2.0 
+
+dotnet build -c JIT_Release -f net45
+dotnet build -c JIT_Release -f netcoreapp2.1 
+dotnet build -c JIT_Release -f netstandard2.0 
+
+pause
