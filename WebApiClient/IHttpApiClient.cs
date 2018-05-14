92b85fb87e172d70b7e83e926e70d20424dfc103
xljiulang <366193849@qq.com>
2018-05-15 00:46:39 +0800

    IHttpApiClient接口Obsolete处理

---
+++ WebApiClient/IHttpApiClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiClient
+{
+    /// <summary>
+    /// 定义HttpApiClient的接口
+    /// </summary>
+    [Obsolete("该接口已废弃，请使用IHttpApi替代", true)]
+    public interface IHttpApiClient : IHttpApi
+    {
+    }
+}
