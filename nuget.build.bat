ad1cf8d03e457b8e845e3b41790dcfddcf30b3ca
xljiulang <366193849@qq.com>
2018-05-21 00:31:48 +0800

    移除Microsoft.Build*.dll的打包

--- nuget.build.bat
+++ nuget.build.bat
@@ -2,8 +2,9 @@ cd WebApiClient
 rd MsBuild\BuildTask /s /q
 
 cd ..\WebApiClient.AOT.Task
-dotnet publish -v q -c AOT_Release -f net45 -o ..\WebApiClient\MsBuild\BuildTask\net45\
-dotnet publish -v q -c AOT_Release -f netcoreapp1.1 -o ..\WebApiClient\MsBuild\BuildTask\netcoreapp1.1\
+dotnet build && dotnet publish -c AOT_Release -f net45 -o ..\WebApiClient\MsBuild\BuildTask\net45\
+dotnet build && dotnet publish -c AOT_Release -f netcoreapp1.1 -o ..\WebApiClient\MsBuild\BuildTask\netcoreapp1.1\
+
 
 cd ..\WebApiClient
 dotnet build -c AOT_Release -f net45
@@ -12,6 +13,8 @@ dotnet build -c AOT_Release -f netstandard1.3
 dotnet build -c JIT_Release -f net45
 dotnet build -c JIT_Release -f netcoreapp2.1 
 dotnet build -c JIT_Release -f netstandard1.3
+
+del MsBuild\BuildTask\net45\Microsoft.Build*.dll
 dotnet pack -c AOT_Release
 dotnet pack -c JIT_Release
 
