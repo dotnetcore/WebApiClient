347989fee539b5aa5208fe68b1709a35a55e0666
xljiulang <366193849@qq.com>
2018-07-25 14:31:17 +0800

    添加异常检测

--- WebApiClient/Internal/UrlBuilder.cs
+++ WebApiClient/Internal/UrlBuilder.cs
@@ -22,18 +22,35 @@ namespace WebApiClient
         /// <summary>
         /// Url创建者
         /// </summary>
-        /// <param name="uri">uri</param>
+        /// <param name="uri">绝对路径的uri</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="UriFormatException"></exception>
+        public UrlBuilder(Uri uri)
+            : this(uri, Encoding.UTF8)
+        {
+        }
+
+        /// <summary>
+        /// Url创建者
+        /// </summary>
+        /// <param name="uri">绝对路径的uri</param>
         /// <param name="encoding">参数的编码</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="UriFormatException"></exception>
         public UrlBuilder(Uri uri, Encoding encoding)
         {
-            this.Uri = uri;
-            this.Encoding = encoding;
+            this.Uri = uri ?? throw new ArgumentNullException(nameof(uri));
+            this.Encoding = encoding ?? throw new ArgumentNullException(nameof(encoding));
+            if (uri.IsAbsoluteUri == false)
+            {
+                throw new UriFormatException($"{nameof(uri)}必须为绝对完整URI");
+            }
         }
 
         /// <summary>
         /// 替换带有花括号的参数的值
         /// </summary>
-        /// <param name="name">参数名称</param>
+        /// <param name="name">参数名称，不带花括号</param>
         /// <param name="value">参数的值</param>
         /// <returns>替换成功则返回true</returns>
         public bool Replace(string name, string value)
@@ -58,8 +75,14 @@ namespace WebApiClient
         /// </summary>
         /// <param name="name">参数名称</param>
         /// <param name="value">参数的值</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void AddQuery(string name, string value)
         {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
             var encode = HttpUtility.UrlEncode(value, this.Encoding);
             var append = $"{name}={encode}";
             var pathQuery = this.Uri.PathAndQuery.TrimEnd('&', '?');
