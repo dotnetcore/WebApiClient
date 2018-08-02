32765f783ddc99ad6580fd03eb155942decdba75
xljiulang <366193849@qq.com>
2018-08-02 09:24:29 +0800

    增加ParameterValidatorTest

---
+++ WebApiClient.Test/Internal/ParameterValidatorTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApiClient;
+using Xunit;
+using System.Linq;
+using WebApiClient.Contexts;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiClient.Test.Internal
+{
+    public class ParameterValidatorTest
+    {
+        class User
+        {
+            [Required]
+            [StringLength(4)]
+            public string Account { get; set; }
+        }
+
+        [Fact]
+        public void ValidateTest()
+        {
+            var parameter = new ApiParameterDescriptor
+            {
+                ValidationAttributes = new[] { new RequiredAttribute() },
+                Name = "user"
+            };
+            Assert.Throws<ValidationException>(() => ParameterValidator.Validate(parameter));
+
+            parameter.Value = new User { };
+            Assert.Throws<ValidationException>(() => ParameterValidator.Validate(parameter));
+
+            parameter.Value = new User { Account = "123" };
+            ParameterValidator.Validate(parameter);
+
+            parameter.Value = new User { Account = "123456" };
+            Assert.Throws<ValidationException>(() => ParameterValidator.Validate(parameter));
+        }
+    }
+}
