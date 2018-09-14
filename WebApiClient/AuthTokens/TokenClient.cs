887c77dc47c947fac3978d052748b64b9cdc4817
xljiulang <366193849@qq.com>
2018-09-14 23:16:34 +0800

    TokenClient静态类的重构

--- WebApiClient/AuthTokens/TokenClient.cs
+++ WebApiClient/AuthTokens/TokenClient.cs
@@ -1,78 +1,68 @@
 ﻿using System;
-using WebApiClient.Defaults;
 
 namespace WebApiClient.AuthTokens
 {
     /// <summary>
-    /// 提供获取ITokenClient的客户端实例
+    /// 提供创建ITokenClient的实例
     /// </summary>
     public static class TokenClient
     {
         /// <summary>
-        /// 授权服务器域名的客户端实例的缓存
+        /// 返回创建ITokenClient的实例
         /// </summary>
-        private static readonly ConcurrentCache<Uri, ITokenClient> cache = new ConcurrentCache<Uri, ITokenClient>();
-
-        /// <summary>
-        /// 返回ITokenClient的客户端实例
-        /// </summary>
-        /// <param name="tokenEndpoint">授权服务器地址</param>
+        /// <param name="tokenEndpoint">提供Token获取的Url节点</param>
         /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="UriFormatException"></exception>
         /// <returns></returns>
+        [Obsolete("此方法已过时，请使用Create方法替代", false)]
         public static ITokenClient Get(string tokenEndpoint)
         {
-            if (string.IsNullOrEmpty(tokenEndpoint))
-            {
-                throw new ArgumentNullException(nameof(tokenEndpoint));
-            }
-
-            var endPoint = new Uri(tokenEndpoint);
-            return TokenClient.Get(endPoint);
+            return Create(tokenEndpoint);
         }
 
         /// <summary>
-        /// 返回ITokenClient的客户端实例
+        /// 返回创建ITokenClient的实例
         /// </summary>
-        /// <param name="tokenEndpoint">授权服务器地址</param>
+        /// <param name="tokenEndpoint">提供Token获取的Url节点</param>
         /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="UriFormatException"></exception>
         /// <returns></returns>
+        [Obsolete("此方法已过时，请使用Create方法替代", false)]
         public static ITokenClient Get(Uri tokenEndpoint)
+        {
+            return Create(tokenEndpoint);
+        }
+
+        /// <summary>
+        /// 返回创建ITokenClient的实例
+        /// </summary>
+        /// <param name="tokenEndpoint">提供Token获取的Url节点</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="UriFormatException"></exception>
+        /// <returns></returns>
+        public static ITokenClient Create(Uri tokenEndpoint)
         {
             if (tokenEndpoint == null)
             {
                 throw new ArgumentNullException(nameof(tokenEndpoint));
             }
-
-            return cache.GetOrAdd(tokenEndpoint, endpoint =>
-            {
-                var config = new HttpApiConfig { HttpHost = endpoint };
-                var apiInterceptor = new NoneDisposeApiInterceptor(config);
-                return HttpApiClient.Create(typeof(ITokenClient), apiInterceptor) as ITokenClient;
-            });
+            return Create(tokenEndpoint.ToString());
         }
 
-
         /// <summary>
-        /// 表示不需要Dispose的拦截器
+        /// 返回创建ITokenClient的实例
         /// </summary>
-        private class NoneDisposeApiInterceptor : ApiInterceptor
+        /// <param name="tokenEndpoint">提供Token获取的Url节点</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="UriFormatException"></exception>
+        /// <returns></returns>
+        public static ITokenClient Create(string tokenEndpoint)
         {
-            /// <summary>
-            /// http接口调用的拦截器
-            /// </summary>
-            /// <param name="httpApiConfig">httpApi配置</param>
-            /// <exception cref="ArgumentNullException"></exception>
-            public NoneDisposeApiInterceptor(HttpApiConfig httpApiConfig)
-                : base(httpApiConfig)
+            if (string.IsNullOrEmpty(tokenEndpoint))
             {
+                throw new ArgumentNullException(nameof(tokenEndpoint));
             }
-
-            /// <summary>
-            /// 不释放
-            /// </summary>
-            public override void Dispose()
-            {
-            }
+            return HttpApiClient.Create<ITokenClient>(tokenEndpoint);
         }
     }
 }
