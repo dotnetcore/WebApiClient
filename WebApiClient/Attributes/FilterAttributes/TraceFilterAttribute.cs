33ab08af86fd78d8dde51f66f5ec63bfeeea88da
xljiulang <366193849@qq.com>
2019-03-13 10:03:02 +0800

    增加TraceFilterBaseAttribute特性

--- WebApiClient/Attributes/FilterAttributes/TraceFilterAttribute.cs
+++ WebApiClient/Attributes/FilterAttributes/TraceFilterAttribute.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
-using System.Text;
 using System.Threading.Tasks;
 using WebApiClient.Contexts;
 
@@ -9,35 +8,12 @@ namespace WebApiClient.Attributes
     /// <summary>
     /// 表示将请求响应内容写入统一日志的过滤器
     /// </summary>
-    public class TraceFilterAttribute : ApiActionFilterAttribute
+    public class TraceFilterAttribute : TraceFilterBaseAttribute
     {
-        /// <summary>
-        /// tag的key
-        /// </summary>
-        private static readonly string tagKey = "$TraceFilter";
-
         /// <summary>
         /// 获取或设置日志的EventId
         /// </summary>
         public int EventId { get; set; }
-
-        /// <summary>
-        /// 获取是否输出请求内容
-        /// </summary>
-        public bool TraceRequest { get; set; } = true;
-
-        /// <summary>
-        /// 获取是否输出响应内容
-        /// </summary>
-        public bool TraceResponse { get; set; } = true;
-
-        /// <summary>
-        /// 将请求响应内容写入统一日志的过滤器
-        /// </summary>
-        public TraceFilterAttribute()
-        {
-            this.OrderIndex = int.MaxValue;
-        }
 
         /// <summary>
         /// 准备请求之前
@@ -46,17 +22,10 @@ namespace WebApiClient.Attributes
         /// <returns></returns>
         public async override Task OnBeginRequestAsync(ApiActionContext context)
         {
-            if (context.HttpApiConfig.LoggerFactory == null)
+            if (context.HttpApiConfig.LoggerFactory != null)
             {
-                return;
+                await base.OnBeginRequestAsync(context).ConfigureAwait(false);
             }
-
-            var request = new Request
-            {
-                Time = DateTime.Now,
-                Message = await context.RequestMessage.GetRequestStringAsync().ConfigureAwait(false)
-            };
-            context.Tags.Set(tagKey, request);
         }
 
         /// <summary>
@@ -66,67 +35,44 @@ namespace WebApiClient.Attributes
         /// <returns></returns>
         public async override Task OnEndRequestAsync(ApiActionContext context)
         {
-            var logging = context.HttpApiConfig.LoggerFactory;
-            if (logging == null)
+            if (context.HttpApiConfig.LoggerFactory != null)
             {
-                return;
-            }
-
-            var builder = new StringBuilder();
-            const string format = "yyyy-MM-dd HH:mm:ss.fff";
-            var request = context.Tags.Get(tagKey).As<Request>();
-
-            if (this.TraceRequest == true)
-            {
-                builder.AppendLine($"[REQUEST] {request.Time.ToString(format)}")
-                    .AppendLine($"{request.Message.TrimEnd()}");
-            }
-
-            var response = context.ResponseMessage;
-            if (this.TraceResponse && response != null && response.Content != null)
-            {
-                if (this.TraceRequest == true)
-                {
-                    builder.AppendLine();
-                }
-
-                builder.AppendLine($"[RESPONSE] {DateTime.Now.ToString(format)}")
-                    .AppendLine($"{await response.Content.ReadAsStringAsync().ConfigureAwait(false)}");
-            }
-
-            var message = builder
-                .AppendLine()
-                .Append($"[TIMESPAN] {DateTime.Now.Subtract(request.Time)}")
-                .ToString();
-
-            var method = context.ApiActionDescriptor.Member;
-            var categoryName = $"{method.DeclaringType.Name}.{method.Name}";
-            var logger = logging.CreateLogger(categoryName);
-
-            if (context.Exception == null)
-            {
-                logger.LogInformation(this.EventId, message);
-            }
-            else
-            {
-                logger.LogError(this.EventId, context.Exception, message);
+                await base.OnEndRequestAsync(context).ConfigureAwait(false);
             }
         }
 
         /// <summary>
-        /// 请求信息
+        /// 输出异常
         /// </summary>
-        private class Request
+        /// <param name="context">上下文</param>
+        /// <param name="exception">异常</param>
+        /// <param name="message">消息</param>
+        protected override void LogError(ApiActionContext context, Exception exception, string message)
         {
-            /// <summary>
-            /// 请求时间
-            /// </summary>
-            public DateTime Time { get; set; }
+            this.GetLogger(context).LogError(this.EventId, exception, message);
+        }
 
-            /// <summary>
-            /// 请求消息
-            /// </summary>
-            public string Message { get; set; }
+        /// <summary>
+        /// 输出消息
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <param name="message">消息</param>
+        protected override void LogInformation(ApiActionContext context, string message)
+        {
+            this.GetLogger(context).LogInformation(this.EventId, message);
+        }
+
+        /// <summary>
+        /// 获取日志组件
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <returns></returns>
+        private ILogger GetLogger(ApiActionContext context)
+        {
+            var logging = context.HttpApiConfig.LoggerFactory;
+            var method = context.ApiActionDescriptor.Member;
+            var categoryName = $"{method.DeclaringType.Name}.{method.Name}";
+            return logging.CreateLogger(categoryName);
         }
     }
 }
---
+++ WebApiClient/Attributes/FilterAttributes/TraceFilterBaseAttribute.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient.Contexts;
+
+namespace WebApiClient.Attributes
+{
+    /// <summary>
+    /// 表示追踪请求响应内容的抽象过滤器
+    /// </summary>
+    public abstract class TraceFilterBaseAttribute : ApiActionFilterAttribute
+    {
+        /// <summary>
+        /// tag的key
+        /// </summary>
+        private static readonly string tagKey = "$TraceFilter";
+
+        /// <summary>
+        /// 时间格式
+        /// </summary>
+        private static readonly string format = "yyyy-MM-dd HH:mm:ss.fff";
+
+        /// <summary>
+        /// 获取是否输出请求内容
+        /// </summary>
+        public bool TraceRequest { get; set; } = true;
+
+        /// <summary>
+        /// 获取是否输出响应内容
+        /// </summary>
+        public bool TraceResponse { get; set; } = true;
+
+        /// <summary>
+        /// 追踪请求响应内容的过滤器
+        /// </summary>
+        public TraceFilterBaseAttribute()
+        {
+            this.OrderIndex = int.MaxValue;
+        }
+
+        /// <summary>
+        /// 准备请求之前
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <returns></returns>
+        public async override Task OnBeginRequestAsync(ApiActionContext context)
+        {
+            var builder = new StringBuilder();
+            if (this.TraceRequest == true)
+            {
+                var requestString = await context.RequestMessage.GetRequestStringAsync().ConfigureAwait(false);
+                builder.AppendLine($"[REQUEST] {DateTime.Now.ToString(format)}").Append(requestString);
+            }
+
+            var request = new Request { Builder = builder, DateTime = DateTime.Now };
+            context.Tags.Set(tagKey, request);
+        }
+
+        /// <summary>
+        /// 请求完成之后
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <returns></returns>
+        public async override Task OnEndRequestAsync(ApiActionContext context)
+        {
+            var response = context.ResponseMessage;
+            var request = context.Tags.Get(tagKey).As<Request>();
+            var builder = request.Builder;
+
+            if (this.TraceResponse && response != null && response.Content != null)
+            {
+                if (builder.Length > 0)
+                {
+                    builder.AppendLine();
+                }
+
+                builder
+                    .AppendLine($"[RESPONSE] {DateTime.Now.ToString(format)}")
+                    .Append(await this.GetResponseStringAsync(response).ConfigureAwait(false));
+            }
+
+            if (builder.Length > 0)
+            {
+                builder.AppendLine();
+            }
+
+            var message = builder
+                .Append($"[TIMESPAN] {DateTime.Now.Subtract(request.DateTime)}")
+                .ToString();
+
+            if (context.Exception == null)
+            {
+                this.LogInformation(context, message);
+            }
+            else
+            {
+                this.LogError(context, context.Exception, message);
+            }
+        }
+
+        /// <summary>
+        /// 返回响应字符串
+        /// </summary>
+        /// <param name="response">响应信息</param>
+        /// <returns></returns>
+        private async Task<string> GetResponseStringAsync(HttpResponseMessage response)
+        {
+            var builder = new StringBuilder();
+            foreach (var item in response.Headers)
+            {
+                builder.AppendLine($"{item.Key}: {string.Join(",", item.Value)}");
+            }
+
+            if (response.Content != null)
+            {
+                foreach (var item in response.Content.Headers)
+                {
+                    builder.AppendLine($"{item.Key}: {string.Join(",", item.Value)}");
+                }
+            }
+
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return builder.AppendLine().Append(content).ToString();
+        }
+
+        /// <summary>
+        /// 输出消息
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <param name="message">消息</param>
+        protected abstract void LogInformation(ApiActionContext context, string message);
+
+        /// <summary>
+        /// 输出异常
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <param name="exception">异常</param>
+        /// <param name="message">消息</param>
+        protected abstract void LogError(ApiActionContext context, Exception exception, string message);
+
+        /// <summary>
+        /// 表示请求记录
+        /// </summary>
+        private class Request
+        {
+            /// <summary>
+            /// 内容
+            /// </summary>
+            public StringBuilder Builder { get; set; }
+
+            /// <summary>
+            /// 时间
+            /// </summary>
+            public DateTime DateTime { get; set; }
+        }
+    }
+}
