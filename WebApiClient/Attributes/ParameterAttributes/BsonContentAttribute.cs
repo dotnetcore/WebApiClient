5ee01fe20d15a885cb3a58e2a265c8836f269374
xljiulang <366193849@qq.com>
2019-03-25 16:10:10 +0800

    增加Bson支持

---
+++ WebApiClient/Attributes/ParameterAttributes/BsonContentAttribute.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WebApiClient.Contexts;
+using WebApiClient.DataAnnotations;
+using WebApiClient.Defaults;
+
+namespace WebApiClient.Attributes
+{
+    /// <summary>
+    /// 序列化参数值得到的bson作为application/bson请求
+    /// 每个Api只能注明于其中的一个参数
+    /// </summary>
+    public class BsonContentAttribute : HttpContentAttribute
+    {
+        /// <summary>
+        /// 设置参数到http请求内容
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <param name="parameter">特性关联的参数</param>
+        protected sealed override Task SetHttpContentAsync(ApiActionContext context, ApiParameterDescriptor parameter)
+        {
+            return base.SetHttpContentAsync(context, parameter);
+        }
+
+        /// <summary>
+        /// 设置参数到http请求内容
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <param name="parameter">特性关联的参数</param>
+        protected sealed override void SetHttpContent(ApiActionContext context, ApiParameterDescriptor parameter)
+        {
+            var options = context.HttpApiConfig.FormatOptions;
+            var setting = this.CreateSerializerSettings(options);
+            var serializer = JsonSerializer.Create(setting);
+
+            var stream = new MemoryStream();
+            var bsonWriter = new BsonWriter(stream);
+            serializer.Serialize(bsonWriter, parameter.Value);
+            stream.Seek(0L, SeekOrigin.Begin);
+
+            var content = new StreamContent(stream);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/bson");
+            context.RequestMessage.Content = content;
+        }
+
+        /// <summary>
+        /// 创建序列化配置     
+        /// </summary>
+        /// <param name="options">格式化选项</param>
+        /// <returns></returns>
+        protected virtual JsonSerializerSettings CreateSerializerSettings(FormatOptions options)
+        {
+            var useCamelCase = options?.UseCamelCase == true;
+            return new JsonSerializerSettings
+            {
+                DateFormatString = options?.DateTimeFormat,
+                ContractResolver = AnnotationsContractResolver.GetResolver(FormatScope.All, useCamelCase)
+            };
+        }
+    }
+}
---
+++ WebApiClient/Attributes/ReturnAttributes/BsonReturnAttribute.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WebApiClient.Contexts;
+using WebApiClient.DataAnnotations;
+using WebApiClient.Defaults;
+
+namespace WebApiClient.Attributes
+{
+    /// <summary>
+    /// 表示使用Bson反序列化回复内容作为返回值
+    /// </summary>
+    public class BsonReturnAttribute : ApiReturnAttribute
+    {
+        /// <summary>
+        /// 配置请求头的accept
+        /// </summary>
+        /// <param name="accept">请求头的accept</param>
+        /// <returns></returns>
+        protected override void ConfigureAccept(HttpHeaderValueCollection<MediaTypeWithQualityHeaderValue> accept)
+        {
+            accept.Add(new MediaTypeWithQualityHeaderValue("application/bson"));
+        }
+
+        /// <summary>
+        /// 获取异步结果
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <exception cref="HttpStatusFailureException"></exception>
+        /// <returns></returns>
+        protected override async Task<object> GetTaskResult(ApiActionContext context)
+        {
+            var options = context.HttpApiConfig.FormatOptions;
+            var setting = this.CreateSerializerSettings(options);
+            var serializer = JsonSerializer.Create(setting);
+
+            var stream = await context.ResponseMessage.Content.ReadAsStreamAsync();
+            var bsonReader = new BsonReader(stream);
+            var dataType = context.ApiActionDescriptor.Return.DataType.Type;
+            return serializer.Deserialize(bsonReader, dataType);
+        }
+
+        /// <summary>
+        /// 创建序列化配置     
+        /// </summary>
+        /// <param name="options">格式化选项</param>
+        /// <returns></returns>
+        protected virtual JsonSerializerSettings CreateSerializerSettings(FormatOptions options)
+        {
+            var useCamelCase = options?.UseCamelCase == true;
+            return new JsonSerializerSettings
+            {
+                DateFormatString = options?.DateTimeFormat,
+                ContractResolver = AnnotationsContractResolver.GetResolver(FormatScope.All, useCamelCase)
+            };
+        }
+    }
+}
