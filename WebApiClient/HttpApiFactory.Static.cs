7d1665812019a99487a20e352001da429abb4fed
xljiulang <366193849@qq.com>
2019-03-26 16:01:57 +0800

    增加HttpApiFactory.Add重载方法

--- WebApiClient/HttpApiFactory.Static.cs
+++ WebApiClient/HttpApiFactory.Static.cs
@@ -50,19 +50,51 @@ namespace WebApiClient
         /// <returns></returns>
         public static HttpApiFactory<TInterface> Add<TInterface>(string name) where TInterface : class, IHttpApi
         {
+            var factory = new HttpApiFactory<TInterface>();
+            return Add(name, factory);
+        }
+
+        /// <summary>
+        /// 添加指定接口的HttpApiFactory
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <param name="factory">工厂实例</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="InvalidOperationException"></exception>
+        /// <returns></returns>
+        public static HttpApiFactory<TInterface> Add<TInterface>(HttpApiFactory<TInterface> factory) where TInterface : class, IHttpApi
+        {
+            var name = GetFactoryName<TInterface>();
+            return Add(name, factory);
+        }
+
+        /// <summary>
+        /// 添加指定接口的HttpApiFactory
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <param name="name">工厂名称</param>
+        /// <param name="factory">工厂实例</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="InvalidOperationException"></exception>
+        /// <returns></returns>
+        public static HttpApiFactory<TInterface> Add<TInterface>(string name, HttpApiFactory<TInterface> factory) where TInterface : class, IHttpApi
+        {
             if (string.IsNullOrEmpty(name) == true)
             {
                 throw new ArgumentNullException(nameof(name));
+            }
+            if (factory == null)
+            {
+                throw new ArgumentNullException(nameof(factory));
             }
 
             lock (syncRoot)
             {
                 if (factories.ContainsKey(name) == true)
                 {
-                    throw new InvalidOperationException($"不允许创建重复名称的HttpApiFactory：{name}");
+                    throw new InvalidOperationException($"不允许添加重复名称的HttpApiFactory：{name}");
                 }
 
-                var factory = new HttpApiFactory<TInterface>();
                 factories.TryAdd(name, factory);
                 return factory;
             }
