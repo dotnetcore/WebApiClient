5084b7789c2ebd3d7940a77adf76f337ac0c9826
xljiulang <366193849@qq.com>
2019-04-01 13:45:00 +0800

    增加netstandard2.0的nuget自动打包

--- nuget.build.bat
+++ nuget.build.bat
@@ -3,10 +3,12 @@ cd WebApiClient
 dotnet build -c AOT_Release -f net45
 dotnet build -c AOT_Release -f netcoreapp2.1
 dotnet build -c AOT_Release -f netstandard1.3
+dotnet build -c AOT_Release -f netstandard2.0
 
 dotnet build -c JIT_Release -f net45
 dotnet build -c JIT_Release -f netcoreapp2.1 
 dotnet build -c JIT_Release -f netstandard1.3
+dotnet build -c JIT_Release -f netstandard2.0
 
 dotnet pack -c AOT_Release
 dotnet pack -c JIT_Release
