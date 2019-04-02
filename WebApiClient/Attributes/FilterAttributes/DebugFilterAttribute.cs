cbf62ba3fe8c534aed2e90b4be13d44d20f9f228
xljiulang <366193849@qq.com>
2019-04-02 14:00:56 +0800

    修复DebugFilterAttribute不输出日志的bug

--- WebApiClient/Attributes/FilterAttributes/DebugFilterAttribute.cs
+++ WebApiClient/Attributes/FilterAttributes/DebugFilterAttribute.cs
@@ -1,67 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Text;
-using System.Threading.Tasks;
 using WebApiClient.Contexts;
 
 namespace WebApiClient.Attributes
 {
     /// <summary>
     /// 表示将请求响应内容打印到输出调试窗口的过滤器
-    /// 仅在DEBUG模式下输出
     /// </summary>
     public class DebugFilterAttribute : TraceFilterBaseAttribute
     {
-        /// <summary>
-        /// 是否为Debug模式
-        /// </summary>
-        private bool isDebugDefined = false;
-
-        /// <summary>
-        /// 将请求响应内容打印到输出调试窗口的过滤器
-        /// 仅在DEBUG模式下输出
-        /// </summary>
-        public DebugFilterAttribute()
-        {
-            this.CheckDebugDefine();
-        }
-
-        /// <summary>
-        /// 检测模式
-        /// </summary>
-        /// <returns></returns>
-        [Conditional("DEBUG")]
-        private void CheckDebugDefine()
-        {
-            this.isDebugDefined = true;
-        }
-
-        /// <summary>
-        /// 准备请求之前
-        /// </summary>
-        /// <param name="context">上下文</param>
-        /// <returns></returns>
-        public async override Task OnBeginRequestAsync(ApiActionContext context)
-        {
-            if (this.isDebugDefined == true)
-            {
-                await base.OnBeginRequestAsync(context).ConfigureAwait(false);
-            }
-        }
-
-        /// <summary>
-        /// 请求完成之后
-        /// </summary>
-        /// <param name="context">上下文</param>
-        /// <returns></returns>
-        public async override Task OnEndRequestAsync(ApiActionContext context)
-        {
-            if (this.isDebugDefined == true)
-            {
-                await base.OnEndRequestAsync(context).ConfigureAwait(false);
-            }
-        }
-
         /// <summary>
         /// 输出异常
         /// </summary>
