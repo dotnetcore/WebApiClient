2686ad5589af4ddb0718b27c16cee9488770815a
陈国伟 <366193849@qq.com>
2019-04-06 14:23:24 +0800

    更新UriEditor的单元测试

--- WebApiClient.Test/Internal/UriEditorTest.cs
+++ WebApiClient.Test/Internal/UriEditorTest.cs
@@ -62,8 +62,9 @@ namespace WebApiClient.Test.Internal
 
             url = new Uri("http://www.webapiclient.com/path/?x={x}&");
             editor = new UriEditor(url, encoding);
-            editor.Replace("x", "你");
+          
             editor.AddQuery("a", "我");
+            editor.Replace("x", "你");
             Assert.True(editor.Uri.ToString() == "http://www.webapiclient.com/path/?x=你&a=我");
 
             url = new Uri("http://www.webapiclient.com");
@@ -71,12 +72,7 @@ namespace WebApiClient.Test.Internal
             editor.AddQuery("a", "我");
             editor.AddQuery("b", "你");
             Assert.True(editor.Uri.ToString() == "http://www.webapiclient.com/?a=我&b=你");
-
-
-            url = new Uri("http://u:p@www.webapiclient.com/x/y/z?a=1&b2=2#tag");
-            editor = new UriEditor(url);
-            editor.SetPath("/");
-            Assert.True(editor.Uri.ToString() == "http://u:p@www.webapiclient.com/?a=1&b2=2#tag");
+             
         }
     }
 }
