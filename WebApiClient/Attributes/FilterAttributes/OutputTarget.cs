d147a0b068a957a8816e4b139ede916407d7172e
xljiulang <366193849@qq.com>
2019-04-08 12:43:15 +0800

    1.0.0-beta3

---
+++ WebApiClient/Attributes/FilterAttributes/OutputTarget.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WebApiClient.Attributes
+{
+    /// <summary>
+    /// 表示输出的目标
+    /// </summary>
+    [Flags]
+    public enum OutputTarget
+    {
+        /// <summary>
+        /// 日志工厂
+        /// </summary>
+        LoggerFactory = 0x1,
+
+        /// <summary>
+        /// 调试窗口
+        /// </summary>
+        Debug = 0x2,
+
+        /// <summary>
+        /// 控制台
+        /// </summary>
+        Console = 0x4,
+
+#if !NETSTANDARD1_3
+        /// <summary>
+        /// 调试器
+        /// </summary>
+        Debugger = 0x8
+#endif
+    }
+}
--- WebApiClient/Attributes/FilterAttributes/TraceFilterAttribute.cs
+++ WebApiClient/Attributes/FilterAttributes/TraceFilterAttribute.cs
@@ -7,35 +7,6 @@ using WebApiClient.Contexts;
 namespace WebApiClient.Attributes
 {
     /// <summary>
-    /// 表示输出的目标
-    /// </summary>
-    [Flags]
-    public enum OutputTarget
-    {
-        /// <summary>
-        /// 日志工厂
-        /// </summary>
-        LoggerFactory = 0x1,
-
-        /// <summary>
-        /// 调试窗口
-        /// </summary>
-        Debug = 0x2,
-
-        /// <summary>
-        /// 控制台
-        /// </summary>
-        Console = 0x4,
-
-#if !NETSTANDARD1_3
-        /// <summary>
-        /// 调试器
-        /// </summary>
-        Debugger = 0x8
-#endif
-    }
-
-    /// <summary>
     /// 表示将请求响应内容指定目标的过滤器
     /// </summary>
     public class TraceFilterAttribute : TraceFilterBaseAttribute
