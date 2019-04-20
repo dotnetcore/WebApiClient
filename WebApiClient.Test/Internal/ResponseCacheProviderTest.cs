8ff25cf9e84e45ece39eee3db5ece213f0853e70
陈国伟 <366193849@qq.com>
2019-04-20 20:29:10 +0800

    增加ResponseCacheProvider的单元测试

---
+++ WebApiClient.Test/Internal/ResponseCacheProviderTest.cs
@@ -0,0 +1,26 @@
+﻿#if !NETSTANDARD1_3
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebApiClient.Test.Internal
+{
+    public class ResponseCacheProviderTest
+    {
+        [Fact]
+        public async Task GetSetAsyncTest()
+        {
+            var cache = await ResponseCacheProvider.Instance.GetAsync("key");
+            Assert.False(cache.HasValue);
+
+            await ResponseCacheProvider.Instance.SetAsync("key", new ResponseCacheEntry(), TimeSpan.FromMilliseconds(20));
+            cache = await ResponseCacheProvider.Instance.GetAsync("key");
+            Assert.True(cache.HasValue);
+
+            await Task.Delay(20);
+            cache = await ResponseCacheProvider.Instance.GetAsync("key");
+            Assert.False(cache.HasValue);
+        }
+    }
+}
+#endif
