0661cc05d9efbd424316e48c396a937f21ee744f
xljiulang <366193849@qq.com>
2019-04-24 09:10:28 +0800

    增加HttpApi.Global

---
+++ WebApiClient/HttpApi.Global.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace WebApiClient
+{
+    /// <summary>
+    /// 提供HttpApi的创建、注册和解析   
+    /// </summary>
+    public partial class HttpApi
+    {
+        /// <summary>
+        /// 一个站点内的默认连接数限制
+        /// </summary>
+        private static int maxConnections = 128;
+
+        /// <summary>
+        /// 获取或设置一个站点内的默认最大连接数
+        /// 这个值在初始化HttpClientHandler时使用
+        /// 默认值为128
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static int MaxConnections
+        {
+            get
+            {
+                return maxConnections;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(MaxConnections));
+                }
+                maxConnections = value;
+            }
+        }
+
+#if NET45
+        /// <summary>
+        /// 获取或设置安全协议版本
+        /// </summary>
+        public static SecurityProtocolType SecurityProtocol
+        {
+            get => ServicePointManager.SecurityProtocol;
+            set => ServicePointManager.SecurityProtocol = value;
+        }
+
+        /// <summary>
+        /// 静态构造函数
+        /// </summary>
+        static HttpApi()
+        {
+            ServicePointManager.SecurityProtocol = Enum
+                .GetValues(typeof(SecurityProtocolType))
+                .Cast<SecurityProtocolType>()
+                .Aggregate((cur, next) => cur | next);
+        }
+#endif         
+    }
+}
