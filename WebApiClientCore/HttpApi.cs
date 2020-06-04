058b933dab391f2347e2e4abbcba2bab63c4ea31
陈国伟 <366193849@qq.com>
2020-06-04 08:57:48 +0800

    使用泛型静态缓存替代接口与代理类的字典缓存

--- WebApiClientCore/HttpApi.cs
+++ WebApiClientCore/HttpApi.cs
@@ -7,46 +7,27 @@ namespace WebApiClientCore
     /// <summary>
     /// 提供创建THttpApi的代理实例
     /// </summary>
-    public static class HttpApi
+    public static class HttpApi<THttpApi>
     {
         /// <summary>
         /// 代理实例创建者的缓存
         /// </summary>
-        private static readonly ConcurrentCache<Type, HttpApiProxyBuilder> proxyBuilderCache = new ConcurrentCache<Type, HttpApiProxyBuilder>();
+        private static readonly HttpApiProxyBuilder proxyBuilder = new HttpApiProxyBuilder(typeof(THttpApi));
 
         /// <summary>
         /// 创建THttpApi的代理实例
-        /// </summary>
-        /// <typeparam name="THttpApi"></typeparam>
+        /// </summary> 
         /// <param name="client">httpClient</param>
         /// <param name="services">服务提供者</param>
         /// <param name="options">配置选项</param>
         /// <exception cref="NotSupportedException"></exception>
         /// <exception cref="ProxyTypeCreateException"></exception>
         /// <returns></returns>
-        public static THttpApi Create<THttpApi>(HttpClient client, IServiceProvider services, HttpApiOptions options)
-        {
-            return (THttpApi)Create(client, services, options, typeof(THttpApi));
-        }
-
-        /// <summary>
-        /// 创建interfaceType的代理实例
-        /// </summary>
-        /// <param name="client">httpClient</param>
-        /// <param name="services">服务提供者</param>
-        /// <param name="options">配置选项</param>
-        /// <param name="interfaceType">接口类型</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="NotSupportedException"></exception>
-        /// <exception cref="ProxyTypeCreateException"></exception>
-        /// <returns></returns>
-        public static object Create(HttpClient client, IServiceProvider services, HttpApiOptions options, Type interfaceType)
+        public static THttpApi Create(HttpClient client, IServiceProvider services, HttpApiOptions options)
         {
             var context = new ServiceContext(client, services, options);
             var interceptor = new ActionInterceptor(context);
-
-            var builder = proxyBuilderCache.GetOrAdd(interfaceType, @interface => new HttpApiProxyBuilder(@interface));
-            return builder.Build(interceptor);
+            return (THttpApi)proxyBuilder.Build(interceptor);
         }
     }
 }
