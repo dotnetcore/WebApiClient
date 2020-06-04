a59e3a0d206db9d738a70165990baddba279565a
xljiulang <366193849@qq.com>
2020-06-04 20:54:30 +0800

    增加创建实例性能测试

---
+++ WebApiClientCore.Benchmarks/CreateInstances/Benchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace WebApiClientCore.Benchmarks.CreateInstances
+{
+    public class Benchmarks : IBenchmark
+    {
+        private readonly Func<int, Model> ctor = Lambda.CreateCtorFunc<int, Model>(typeof(Model));
+
+        [Benchmark]
+        public void ActivatorCreate()
+        {
+            typeof(Model).CreateInstance<Model>(1);
+        }
+
+        [Benchmark]
+        public void LabdaCreate()
+        {
+            ctor.Invoke(1);
+        }
+
+        public class Model
+        {
+            private readonly int value;
+
+            public Model(int value)
+            {
+                this.value = value;
+            }
+        }
+    }
+}
