8cad12d48f0b6ff09209030010616556848580e5
xljiulang <366193849@qq.com>
2020-06-07 03:22:28 +0800

    ActionInterceptor嵌套到HttpApi静态类

--- WebApiClientCore/HttpApi.cs
+++ WebApiClientCore/HttpApi.cs
@@ -51,5 +51,37 @@ namespace WebApiClientCore
         {
             return HttpApiProxyBuilder<THttpApi>.Build(actionInterceptor);
         }
+
+
+        /// <summary>
+        /// 表示httpApi方法调用的拦截器
+        /// </summary>
+        private class ActionInterceptor : IActionInterceptor
+        {
+            /// <summary>
+            /// 服务上下文
+            /// </summary>
+            private readonly HttpClientContext context;
+
+            /// <summary>
+            /// httpApi方法调用的拦截器
+            /// </summary>
+            /// <param name="context">服务上下文</param> 
+            public ActionInterceptor(HttpClientContext context)
+            {
+                this.context = context;
+            }
+
+            /// <summary>
+            /// 拦截方法的调用
+            /// </summary>
+            /// <param name="actionInvoker">action执行器</param> 
+            /// <param name="arguments">方法的参数集合</param>
+            /// <returns></returns>
+            public object Intercept(IActionInvoker actionInvoker, object?[] arguments)
+            {
+                return actionInvoker.Invoke(this.context, arguments);
+            }
+        }
     }
 }
