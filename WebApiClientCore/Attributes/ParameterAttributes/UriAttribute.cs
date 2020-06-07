b70e6b9550ecd65242d9f2bcefbc4f67f1e1cb2d
xljiulang <366193849@qq.com>
2020-06-08 00:35:15 +0800

    优化UriAttribute，当参数为Uri类型时无需二次转换为Uri

--- WebApiClientCore/Attributes/ParameterAttributes/UriAttribute.cs
+++ WebApiClientCore/Attributes/ParameterAttributes/UriAttribute.cs
@@ -19,8 +19,7 @@ namespace WebApiClientCore.Attributes
         /// <returns></returns>
         public override Task OnRequestAsync(ApiParameterContext context)
         {
-            var uriString = context.ParameterValue?.ToString();
-            if (uriString == null)
+            if (context.ParameterValue == null)
             {
                 throw new ArgumentNullException(context.Parameter.Name);
             }
@@ -30,10 +29,14 @@ namespace WebApiClientCore.Attributes
                 throw new ApiInvalidConfigException(Resx.invalid_UriAttribute);
             }
 
-            var relative = new Uri(uriString, UriKind.RelativeOrAbsolute);
-            if (relative.IsAbsoluteUri == true)
+            if (!(context.ParameterValue is Uri uri))
             {
-                context.HttpContext.RequestMessage.RequestUri = relative;
+                uri = new Uri(context.ParameterValue.ToString(), UriKind.RelativeOrAbsolute);
+            }
+
+            if (uri.IsAbsoluteUri == true)
+            {
+                context.HttpContext.RequestMessage.RequestUri = uri;
             }
             else
             {
@@ -42,7 +45,7 @@ namespace WebApiClientCore.Attributes
                 {
                     throw new ApiInvalidConfigException(Resx.required_HttpHost);
                 }
-                context.HttpContext.RequestMessage.RequestUri = new Uri(baseUri, relative);
+                context.HttpContext.RequestMessage.RequestUri = new Uri(baseUri, uri);
             }
             return Task.CompletedTask;
         }
