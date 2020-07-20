5f00a4699d5932054be871d1f90b35b9da39381c
陈国伟 <366193849@qq.com>
2020-07-20 17:46:06 +0800

    代码生成器迁移到.netcore

---
+++ WebApiClientCore.OpenApi.SourceGenerator/HtmlTempate.cs
@@ -0,0 +1,33 @@
+﻿using RazorEngineCore;
+
+namespace WebApiClientCore.OpenApi.SourceGenerator
+{
+    /// <summary>
+    /// html模板
+    /// </summary>
+    public class HtmlTempate : RazorEngineTemplateBase
+    {
+        /// <summary>
+        /// html标签转换
+        /// </summary>
+        /// <param name="obj"></param>
+        public override void Write(object obj = null)
+        {
+            var text = obj?.ToString();
+            if (text != null)
+            {
+                text = text.Replace("<", "&lt;").Replace(">", "&gt;");
+            }
+            base.Write(text);
+        }
+    }
+
+    /// <summary>
+    /// html模板
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class HtmlTempate<T> : HtmlTempate
+    {
+        public new T Model { get; set; }
+    }
+}
