ecf9e5464633a050c0f358c5fc013defce1f6e77
陈国伟 <366193849@qq.com>
2020-09-08 09:14:53 +0800

    移除HttpClientBuilderExtensions

--- WebApiClientCore/Microsoft.Extensions/DependencyInjection/HttpApiConfigureExtensions.cs
+++ WebApiClientCore/Microsoft.Extensions/DependencyInjection/HttpApiConfigureExtensions.cs
@@ -58,5 +58,28 @@ namespace Microsoft.Extensions.DependencyInjection
             var name = httpApiType.FullName;
             return services.Configure<HttpApiOptions>(name, configureOptions);
         }
+
+        /// <summary>
+        /// 配置HttpApi的选项
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="configureOptions">配置选项</param>
+        public static IHttpClientBuilder ConfigureHttpApi(this IHttpClientBuilder builder, Action<HttpApiOptions> configureOptions)
+        {
+            builder.Services.Configure(builder.Name, configureOptions);
+            return builder;
+        }
+
+        /// <summary>
+        /// 配置HttpApi的选项
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="configureOptions">配置选项</param>
+        /// <returns></returns>
+        public static IHttpClientBuilder ConfigureHttpApi(this IHttpClientBuilder builder, IConfiguration configureOptions)
+        {
+            builder.Services.Configure<HttpApiOptions>(builder.Name, configureOptions);
+            return builder;
+        }
     }
 }
