80cf40528f4243af7a018e6af1f3a5686ca6b67a
xljiulang <366193849@qq.com>
2021-01-24 02:58:18 +0800

    修改注释

--- WebApiClientCore/IHttpApi.cs
+++ WebApiClientCore/IHttpApi.cs
@@ -2,8 +2,11 @@
 {
     /// <summary>
     /// 定义HttpApi标识的接口
-    /// 继承此接口将获得智能语法分析提示的能力
     /// </summary>
+    /// <remarks>
+    /// <para>• 1.x版本，继承此接口才获得语法分析提示</para>
+    /// <para>• 2.0以后的版本，不继承此接口也获得语法分析提示</para>
+    /// </remarks>
     public interface IHttpApi
     {
     }
