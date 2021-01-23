e3c9ca9a5025e11d344cffe5d7761918520d68cd
xljiulang <366193849@qq.com>
2021-01-23 22:44:29 +0800

    完善注释

--- WebApiClientCore.Extensions.SourceGenerator/DependencyInjection/WebApiClientBuilderExtensions.cs
+++ WebApiClientCore.Extensions.SourceGenerator/DependencyInjection/WebApiClientBuilderExtensions.cs
@@ -12,6 +12,13 @@ namespace Microsoft.Extensions.DependencyInjection
         /// 编译时使用SourceGenerator生成接口的代理类型代码
         /// 运行时查找接口的代理类型并创建实例
         /// </summary>
+        /// <remarks>
+        /// <para>• 要求Visual Studio或msbuild工具为最新版本才支持SourceGenerator技术</para>
+        /// <para>• 要求声明http接口的项目必须引用WebApiClientCore.Extensions.SourceGenerator包</para>
+        /// <para>• 目前SourceGenerator技术不支持partial分散在多个cs文件的单个http接口</para>
+        /// <para>• 目前SourceGenerator技术只支持c#语言</para>
+        /// <para>• 使用SourceGenerator相比Emit创建代理类并不能带来性能提升</para>
+        /// </remarks>
         /// <param name="builder"></param>
         /// <returns></returns>
         public static IWebApiClientBuilder UseSourceGeneratorHttpApiActivator(this IWebApiClientBuilder builder)
