26227e92132600897f75fe0a661ec841569e4992
陈国伟 <366193849@qq.com>
2024-05-31 14:07:28 +0800

    fix demo

--- AppAot/AppHostedService.cs
+++ AppAot/AppHostedService.cs
@@ -1,35 +1,26 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using WebApiClientCore.Extensions.OAuths;
 
 namespace AppAot
 {
     class AppHostedService : BackgroundService
-    {
-        private readonly ITokenProviderFactory tokenProviderFactory;
+    { 
         private readonly IServiceScopeFactory serviceScopeFactory;
         private readonly ILogger<AppHostedService> logger;
 
-        public AppHostedService(
-            ITokenProviderFactory tokenProviderFactory,
+        public AppHostedService( 
             IServiceScopeFactory serviceScopeFactory,
             ILogger<AppHostedService> logger)
-        {
-            this.tokenProviderFactory = tokenProviderFactory;
+        { 
             this.serviceScopeFactory = serviceScopeFactory;
             this.logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            var p = this.tokenProviderFactory.Create(typeof(ICloudflareApi));
+        { 
             using var scope = this.serviceScopeFactory.CreateScope();
             var api = scope.ServiceProvider.GetRequiredService<ICloudflareApi>();
             var appData = await api.GetAppDataAsync();
