18226c8eaa2c434849de6748d0ab488298da5599
陈国伟 <366193849@qq.com>
2024-06-18 15:12:49 +0800

    使用静态构造器

--- WebApiClientCore.Benchmarks/Requests/MockResponseHandler.cs
+++ WebApiClientCore.Benchmarks/Requests/MockResponseHandler.cs
@@ -11,17 +11,25 @@ namespace WebApiClientCore.Benchmarks.Requests
     /// </summary>
     class MockResponseHandler : DelegatingHandler
     {
-        private readonly byte[] json;
+        private static readonly byte[] jsonByteArray;
 
-        public MockResponseHandler()
+        static MockResponseHandler()
         {
-            var model = new Model { A = "A", B = 2, C = 3d };
-            this.json = JsonSerializer.SerializeToUtf8Bytes(model);
+            var model = new Model
+            {
+                A = "A",
+                B = 2,
+                C = 3d
+            };
+            jsonByteArray = JsonSerializer.SerializeToUtf8Bytes(model);
         }
 
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new ByteArrayJsonContent(this.json) };
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ByteArrayJsonContent(jsonByteArray)
+            };
             return Task.FromResult(response);
         }
     }
