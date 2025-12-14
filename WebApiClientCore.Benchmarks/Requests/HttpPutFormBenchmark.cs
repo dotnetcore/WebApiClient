12d42bb01821fc0a6e517f3730d207847c701840
Ezreal <memory_of_you@hotmail.com>
2025-12-14 12:26:45 +0800

    feat(benchmarks): 添加 RestSharp 基准测试支持
    添加 RestSharp 作为新的 HTTP 客户端基准测试工具
    在多个请求类型中实现 RestSharp 的基准测试方法
    配置 RestSharp 客户端服务并添加相关包装类

--- WebApiClientCore.Benchmarks/Requests/HttpPutFormBenchmark.cs
+++ WebApiClientCore.Benchmarks/Requests/HttpPutFormBenchmark.cs
@@ -1,5 +1,6 @@
-﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes;
 using Microsoft.Extensions.DependencyInjection;
+using RestSharp;
 using System.Threading.Tasks;
 
 namespace WebApiClientCore.Benchmarks.Requests
@@ -20,6 +21,22 @@ namespace WebApiClientCore.Benchmarks.Requests
             using var scope = this.ServiceProvider.CreateScope();
             var benchmarkApi = scope.ServiceProvider.GetRequiredService<IRefitJsonApi>();
             return await benchmarkApi.PutFormAsync(id: "id001", User.Instance);
+        }
+
+        [Benchmark]
+        public async Task<User> RestSharp_PutFormAsync()
+        {
+            using var scope = this.ServiceProvider.CreateScope();
+            var client = scope.ServiceProvider.GetRequiredService<RestSharpJsonClient>();
+            var request = new RestRequest($"/benchmarks/id001")
+                .AddParameter("id", User.Instance.Id)
+                .AddParameter("name", User.Instance.Name)
+                .AddParameter("bio", User.Instance.Bio)
+                .AddParameter("followers", User.Instance.Followers)
+                .AddParameter("following", User.Instance.Following)
+                .AddParameter("url", User.Instance.Url)
+                .AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            return await client.PutAsync<User>(request);
         }
     }
 }
